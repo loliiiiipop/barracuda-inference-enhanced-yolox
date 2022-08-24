47579879fc7fee83f42e2a782de9e620a308007a
loliiiiipop <loliiiiipop@proton.me>
2022-08-24 16:06:20 +0000

    cc

--- Editor/PackageInstaller.cs
+++ Editor/PackageInstaller.cs
@@ -19,3 +19,24 @@ namespace CJM.BarracudaInference.YOLOX
 
     // Serializable class to hold a list of PackageData objects
     [System.Serializable]
+    public class PackageList
+    {
+        public List<PackageData> packages;
+    }
+
+    public class PackageInstaller
+    {
+        // Stores the AddRequest object for the current package to install.
+        private static AddRequest addRequest;
+        // A list of PackageData objects to install.
+        private static List<PackageData> packagesToInstall;
+        // The index of the current package to install.
+        private static int currentPackageIndex;
+
+        // GUID of the JSON file containing the list of packages to install
+        private const string PackagesJSONGUID = "02aec9cd479b4b758a7afde0032230ec";
+
+        // Method called on load to install packages from the JSON file
+        [InitializeOnLoadMethod]
+        public static void InstallDependencies()
+        {
