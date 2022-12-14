d02de5e7768b61e347e945f97c3315df3916b340
loliiiiipop <loliiiiipop@proton.me>
2022-12-14 19:35:18 +0000

    Update build-constraints.yaml

--- Runtime/Scripts/YOLOXObjectDetector.cs
+++ Runtime/Scripts/YOLOXObjectDetector.cs
@@ -14,3 +14,22 @@ namespace CJM.BarracudaInference.YOLOX
 {
     /// <summary>
     /// YOLOXObjectDetector is a class that extends BarracudaModelRunner for object detection using the YOLOX model.
+    /// It handles model execution, processes the output, and generates bounding boxes with corresponding labels and colors.
+    /// The class supports various worker types, including PixelShader and ComputePrecompiled, as well as Async GPU Readback.
+    /// </summary>
+    public class YOLOXObjectDetector : BarracudaModelRunner
+    {
+        // Output Processing configuration and variables
+        [Header("Output Processing")]
+        // JSON file containing the color map for bounding boxes
+        [SerializeField, Tooltip("JSON file with bounding box colormaps")]
+        private TextAsset colormapFile;
+
+        [Header("Settings")]
+        [Tooltip("Interval (in frames) for unloading unused assets with Pixel Shader backend")]
+        [SerializeField] private int pixelShaderUnloadInterval = 100;
+
+        // A counter for the number of frames processed.
+        private int frameCounter = 0;
+
+        // Indicates if the system supports asynchronous GPU readback
